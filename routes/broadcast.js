2c68a37fa9150066f691d83dcfda9310c5763fc7
Andrew Arpasi <andrew@andrewarpasi.com>
2017-07-14 22:40:19 -0400

    Method to stop a broadcast by scene

--- routes/broadcast.js
+++ routes/broadcast.js
@@ -22,6 +22,7 @@ api.put('/broadcast/:broadcast_id/scene/:scene_id',function(req,res) {
         if(routes.handleErrors(err,scene,'Scene',res)) return;
         Broadcast.findOneAndUpdate({_id: broadcastId},{scene: id},{new: true},function(err1,broadcast) {
             if(routes.handleErrors(err1,broadcast,'Broadcast',res)) return;
+            //TODO: update scene over socket.io
             res.json(broadcast);
         });
     });
@@ -33,12 +34,21 @@ api.delete('/broadcast/:broadcast_id',function(req,res) {
     const broadcastId = req.params.broadcast_id;
     Broadcast.findOneAndRemove({_id: broadcastId},function(err,broadcast,result) {
         if(routes.handleErrors(err,broadcast,'Broadcast',res)) return;
+        //TODO: send stop broadcast over socket.io
         res.json(broadcast);
     });
 });
 
 //stop a broadcast by scene
 api.delete('/scene/:scene_id/broadcast',function(req,res) {
-    
+    const id = req.params.scene_id;
+    Scene.findById(id, function (err, scene) {
+        if(routes.handleErrors(err,scene,'Scene',res)) return;
+        Broadcast.findOneAndRemove({scene: id},function(err1,broadcast,result) {
+            if(routes.handleErrors(err1,broadcast,'Broadcast',res)) return;
+            //TODO: send stop broadcast over socket.io
+            res.json(broadcast);
+        });
+    });
 });
 
